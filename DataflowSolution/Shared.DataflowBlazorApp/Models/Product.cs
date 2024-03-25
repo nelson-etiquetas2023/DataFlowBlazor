e8691eef6574136187d6f7c75407a33f601969fd
Nelson Pino <devsoftware.etiquetas@gmail.com>
2024-03-25 16:15:30 -0400

    creacion de la pagina de productos

---
+++ DataflowSolution/Shared.DataflowBlazorApp/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DataflowBlazorApp.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public double Price { get; set; }
+        public string Category { get; set; } = null!;
+        public string Presentation { get; set; } = null!;
+
+    }
+}
